325368436d81864debfdad156dc373d2fc43f228
Waheed Prajadisastra <waheedosx@gmail.com>
2019-11-19 16:36:43 +0700

    include build, deleted /build in .gitignore

---
+++ build/precache-manifest.676f46188f045333409a8a765bd339b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2344f0a3f42b618845c8e5672e63a6dd",
+    "url": "/furniture-finder-react/index.html"
+  },
+  {
+    "revision": "fd7632c30bb4497ef450",
+    "url": "/furniture-finder-react/static/js/2.be62a8e5.chunk.js"
+  },
+  {
+    "revision": "a21824641886db5a4cb7",
+    "url": "/furniture-finder-react/static/js/main.e20b9ed9.chunk.js"
+  },
+  {
+    "revision": "b72c1e7b0af1a98d542e",
+    "url": "/furniture-finder-react/static/js/runtime-main.42fb9a26.js"
+  }
+]);
